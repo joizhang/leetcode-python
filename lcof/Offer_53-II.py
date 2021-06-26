6eac8fca8c06bded2a475700f03c824d13935886
joizhang <zxlwhom@outlook.com>
2021-06-26 18:29:42 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ lcof/Offer_53-II.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if nums[mid] == mid:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.missingNumber([0]))
+    print(s.missingNumber([0, 2]))
+    print(s.missingNumber([0, 1, 3]))
+    print(s.missingNumber([0, 1, 2, 3, 4, 5, 6, 7, 9]))
