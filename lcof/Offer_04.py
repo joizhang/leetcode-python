73630406a54d3fd41d9a63f331001f5cbc8de1f1
joizhang <zxlwhom@outlook.com>
2021-06-26 11:34:13 +0800

    剑指 Offer 04. 二维数组中的查找 LCOF

---
+++ lcof/Offer_04.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target:
+                i -= 1
+            elif matrix[i][j] < target:
+                j += 1
+            else:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+    mat = [
+        [1, 4, 7, 11, 15],
+        [2, 5, 8, 12, 19],
+        [3, 6, 9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30]
+    ]
+    print(s.findNumberIn2DArray(mat, 5))
