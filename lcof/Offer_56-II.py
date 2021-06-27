0a803898115fbad781d4c64ad596e5c64e218c8f
joizhang <zxlwhom@outlook.com>
2021-06-27 17:48:39 +0800

    剑指 Offer 56 - II. 数组中数字出现的次数 II

---
+++ lcof/Offer_56-II.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        one, two = 0, 0
+        for num in nums:
+            one = one ^ num & ~two
+            two = two ^ num & ~one
+        return one
