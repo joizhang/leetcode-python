9d85a8e422942e5b85ca0c17d8f2b8b4dca20def
joizhang <zxlwhom@outlook.com>
2021-06-27 17:49:25 +0800

    剑指 Offer 58 - II. 左旋转字符串

---
+++ lcof/Offer_58-II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, s, start, end):
+        while start < end:
+            tmp = s[start]
+            s[start] = s[end]
+            s[end] = tmp
+            start += 1
+            end -= 1
+
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        ss = [x for x in s]
+        self.reverse(ss, 0, n - 1)
+        self.reverse(ss, n, len(s) - 1)
+        self.reverse(ss, 0, len(s) - 1)
+        return ''.join(ss)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseLeftWords('abcdefg', 2))
