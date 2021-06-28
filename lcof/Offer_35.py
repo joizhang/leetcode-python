c474ef69f7f0aaa57bdc9beb777ff095da37a6eb
joizhang <zxlwhom@outlook.com>
2021-06-28 19:37:05 +0800

    剑指 Offer 35. 复杂链表的复制

---
+++ lcof/Offer_35.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+from utils import Node
+
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head:
+            return None
+        dd = {}
+        cur = head
+        while cur:
+            dd[cur] = Node(cur.val)
+            cur = cur.next
+
+        cur = head
+        while cur:
+            dd[cur].next = dd.get(cur.next)
+            dd[cur].random = dd.get(cur.random)
+            cur = cur.next
+        return dd[head]
