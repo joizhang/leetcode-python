a96ca05d0643048adef0f1270c122b5392b8196c
joizhang <zxlwhom@outlook.com>
2021-06-30 16:38:32 +0800

    剑指 Offer 15. 二进制中1的个数

---
+++ lcof/Offer_15.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # 若n&1=0，则n二进制最右一位为0
+        # 若n&1=0，则n二进制最右一位为1
+        ans = 0
+        while n:
+            ans += n & 1
+            n >>= 1
+        return ans
