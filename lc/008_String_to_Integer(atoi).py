7e7f110aacad45af43f87022baa2f87ce74cccf5
joizhang <zxlwhom@outlook.com>
2021-07-03 20:28:48 +0800

    008_String_to_Integer(atoi)

--- lc/008_String_to_Integer(atoi).py
+++ lc/008_String_to_Integer(atoi).py
@@ -4,10 +4,12 @@ INT_MIN_DIV_TEN = -214748364
 
 class Solution:
     def myAtoi(self, s: str) -> int:
+        # 去除两边空格
         s = s.strip()
         if len(s) == 0:
             return 0
         n, ans, symbol, i = len(s), 0, 1, 0
+        # 记录正负号
         if s[i] == '-':
             symbol = -1
             i += 1
@@ -20,8 +22,10 @@ class Solution:
             if s[i] == ' ':
                 continue
             num = symbol * int(s[i])
+            # 判断是否会超过有符号整型最大值
             if ans > INT_MAX_DIV_TEN or (ans == INT_MAX_DIV_TEN and num > 7):
                 return 2 ** 31 - 1
+            # 判断是否会超过有符号整型最小值
             if ans < INT_MIN_DIV_TEN or (ans == INT_MIN_DIV_TEN and num < -8):
                 return -2 ** 31
             ans = ans * 10 + num
