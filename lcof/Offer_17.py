1c9d5b8d7eb17d65c1a4eebd53ab7e7acf146792
joizhang <zxlwhom@outlook.com>
2021-07-03 15:39:49 +0800

    剑指 Offer 17. 打印从1到最大的n位数

--- lcof/Offer_17.py
+++ lcof/Offer_17.py
@@ -2,19 +2,28 @@ from typing import List
 
 
 class Solution:
+    nine = 0
+    start = 0
 
-    def backtrack(self, n, track, ans):
-        if len(track) == n:
-            ans.append(''.join(track))
+    def backtrack(self, n, x, num, ans):
+        if x == n:
+            s = ''.join(num[self.start:])
+            if s != '0':
+                ans.append(int(s))
+            if n - self.start == self.nine:
+                self.start -= 1
             return
-
         for i in range(10):
-            track.append(str(i))
-            self.backtrack(n, track, ans)
+            if i == 9:
+                self.nine += 1
+            num[x] = str(i)
+            self.backtrack(n, x + 1, num, ans)
+        self.nine -= 1
 
     def printNumbers(self, n: int) -> List[int]:
-        ans = []
-        self.backtrack(n, [], ans)
+        num, ans = ['0'] * n, []
+        self.nine, self.start = 0, n - 1
+        self.backtrack(n, 0, num, ans)
         return ans
 
 
