5ab618ece83987ff6489a75bd804ff320a8ea531
joizhang <zxlwhom@outlook.com>
2021-07-04 20:09:10 +0800

    92. 反转链表 II

--- lc/092_Reverse_Linked_Lis_II.py
+++ lc/092_Reverse_Linked_Lis_II.py
@@ -1,15 +1,45 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-from utils import ListNode
+from utils import ListNode, list_to_linklist, linklist_to_list
 
 
 class Solution:
+    def reverse(self, head):
+        if not head:
+            return head
+        p = None
+        cur = head
+        while head:
+            head = head.next
+            cur.next = p
+            p = cur
+            cur = head
+        return p
+
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        pass
+        if not head:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        pre, end = dummy, dummy
+        i = 0
+        while i < left - 1:
+            pre = pre.next
+            i += 1
+        i = 0
+        while i < right:
+            end = end.next
+            i += 1
+
+        start = pre.next
+        end_next = end.next
+
+        end.next = None
+        pre.next = self.reverse(start)
+        start.next = end_next
+
+        return dummy.next
 
 
 if __name__ == '__main__':
     s = Solution()
+    l = list_to_linklist([1, 2, 3, 4, 5])
+    print(linklist_to_list(s.reverseBetween(l, 2, 4)))
