f2407e123045f8b8a736f8106547f9ae2cb262a1
joizhang <zxlwhom@outlook.com>
2021-07-07 15:58:13 +0800

    121_Best_Time_to_Buy_and_Sell_Stock

---
+++ lc/dp/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cost, profit = float('+inf'), 0
+        for price in prices:
+            cost = min(cost, price)
+            profit = max(profit, price - cost)
+        return profit
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxProfit([7, 1, 5, 3, 6, 4]))
+    print(s.maxProfit([7, 6, 4, 3, 1]))
