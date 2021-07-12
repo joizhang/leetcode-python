81e2cf234feff8159b2cc5c6ef49f5d53dcdd80c
joizhang <zxlwhom@outlook.com>
2021-07-12 14:15:07 +0800

    剑指 Offer 17. 打印从1到最大的n位数

--- lcof/Offer_17.py
+++ lcof/Offer_17.py
@@ -27,6 +27,37 @@ class Solution:
         return ans
 
 
+class Solution2:
+    nine = 0
+    start = 0
+
+    def backtrack(self, n, x, num, ans):
+        if x == n:
+            s = ''.join(num[self.start:])
+            if s != '0':
+                ans.append(s)
+            if n - self.start == self.nine:
+                self.start -= 1
+            return
+
+        for i in range(10):
+            if i == 9:
+                self.nine += 1
+            num[x] = str(i)
+            self.backtrack(n, x + 1, num, ans)
+        self.nine -= 1
+
+    def printNumbers(self, n: int) -> List[int]:
+        num, ans = ['0'] * n, []
+        self.nine = 0
+        self.start = n - 1
+        self.backtrack(n, 0, num, ans)
+        return ans
+
+
 if __name__ == '__main__':
-    s = Solution()
-    print(s.printNumbers(2))
+    # s = Solution()
+    # print(s.printNumbers(2))
+
+    s = Solution2()
+    print(s.printNumbers(3))
