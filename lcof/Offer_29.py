e94f623f6246c08341548ee9547027a3b339904d
joizhang <zxlwhom@outlook.com>
2021-07-13 10:15:16 +0800

    剑指 Offer 29. 顺时针打印矩阵

--- lcof/Offer_29.py
+++ lcof/Offer_29.py
@@ -4,38 +4,36 @@ from typing import List
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         ans = []
-        if not matrix:
-            return ans
-        l, r, t, b = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
+        left, right, top, bottom = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
         while True:
-            for i in range(l, r + 1):
-                ans.append(matrix[t][i])
-            t += 1
-            if t > b:
+            for i in range(left, right + 1):
+                ans.append(matrix[top][i])
+            top += 1
+            if top > bottom:
                 break
 
-            for i in range(t, b + 1):
-                ans.append(matrix[i][r])
-            r -= 1
-            if l > r:
+            for i in range(top, bottom + 1):
+                ans.append(matrix[i][right])
+            right -= 1
+            if left > right:
                 break
 
-            for i in reversed(range(l, r + 1)):
-                ans.append(matrix[b][i])
-            b -= 1
-            if t > b:
+            for i in reversed(range(left, right + 1)):
+                ans.append(matrix[bottom][i])
+            bottom -= 1
+            if top > bottom:
                 break
 
-            for i in reversed(range(t, b + 1)):
-                ans.append(matrix[i][l])
-            l += 1
-            if l > r:
+            for i in reversed(range(top, bottom + 1)):
+                ans.append(matrix[i][left])
+            left += 1
+            if left > right:
                 break
         return ans
 
 
 if __name__ == '__main__':
-    s = Solution()
+    s = Solution2()
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     print(s.spiralOrder(matrix))
 
