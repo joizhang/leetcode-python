8bf3192bbd131c960e64948b5381d47aa00b94f7
joizhang <zxlwhom@outlook.com>
2021-07-13 10:14:42 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

--- lcof/Offer_21.py
+++ lcof/Offer_21.py
@@ -2,24 +2,26 @@ from typing import List
 
 
 class Solution:
+
     def exchange(self, nums: List[int]) -> List[int]:
         lo, hi = 0, len(nums) - 1
         while lo < hi:
-            # 从左往右找偶数
-            while lo < hi:
-                if nums[lo] % 2 == 0:
-                    break
+            while lo < hi and nums[lo] % 2 == 1:
                 lo += 1
-
-            while lo < hi:
-                if nums[hi] % 2 != 0:
-                    break
+            while lo < hi and nums[hi] % 2 == 0:
                 hi -= 1
-
             nums[lo], nums[hi] = nums[hi], nums[lo]
+            lo += 1
+            hi -= 1
         return nums
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.exchange([1,2,3,4]))
+    print(s.exchange([2, 1]))
+    print(s.exchange([1, 2, 3]))
+    print(s.exchange([1, 3, 2]))
+    print(s.exchange([3, 2, 1]))
+    print(s.exchange([1, 2, 3, 4]))
+    print(s.exchange([4, 3, 2, 1]))
+    print(s.exchange([1, 3, 2, 4]))
