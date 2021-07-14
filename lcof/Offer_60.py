0341dc815e1676ce767165989fb2e673a352e22e
joizhang <zxlwhom@outlook.com>
2021-07-14 17:21:44 +0800

    剑指 Offer 60. n个骰子的点数

---
+++ lcof/Offer_60.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def dicesProbability(self, n: int) -> List[float]:
+        dp = [1 / 6] * 6
+        for i in range(2, n + 1):
+            tmp = [0] * (5 * i + 1)
+            for j in range(len(dp)):
+                for k in range(6):
+                    tmp[j + k] += dp[j] / 6
+            dp = tmp
+        return dp
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.dicesProbability(2))
