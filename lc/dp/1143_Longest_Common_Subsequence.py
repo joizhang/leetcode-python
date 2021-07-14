0fff699f60bcbc1123ae7cfb5657a8e92c0abddd
joizhang <zxlwhom@outlook.com>
2021-07-14 19:35:23 +0800

    1143_Longest_Common_Subsequence

--- lc/dp/1143_Longest_Common_Subsequence.py
+++ lc/dp/1143_Longest_Common_Subsequence.py
@@ -1,17 +1,19 @@
 class Solution:
 
     def dp(self, text1, text2, i, j):
-        if i == -1 or j == -1:
+        # base case
+        if i == len(text1) or j == len(text2):
             return 0
         elif text1[i] == text2[j]:
-            return self.dp(text1, text2, i - 1, j - 1) + 1
+            # text1[i] 和 text2[j] 必然在 lcs 中，加上 text1[i+1..] 和 text2[j+1..] 中的 lcs 长度，就是答案
+            return 1 + self.dp(text1, text2, i + 1, j + 1)
         else:
-            return max(self.dp(text1, text2, i - 1, j), self.dp(text1, text2, i, j - 1))
-
-    def longestCommonSubsequence2(self, text1: str, text2: str) -> int:
-        return self.dp(text1, text2, len(text1) - 1, len(text2) - 1)
+            return max(self.dp(text1, text2, i + 1, j), self.dp(text1, text2, i, j + 1))
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return self.dp(text1, text2, 0, 0)
+
+    def longestCommonSubsequence2(self, text1: str, text2: str) -> int:
         m, n = len(text1), len(text2)
         dp = [[0] * (n + 1) for _ in range(m + 1)]
         for i in range(1, m + 1):
