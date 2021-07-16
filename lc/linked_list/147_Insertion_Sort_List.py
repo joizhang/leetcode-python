db045361443db689767e415c99ed800db550b491
joizhang <zxlwhom@outlook.com>
2021-07-16 12:24:25 +0800

    147_Insertion_Sort_List

--- lc/linked_list/147_Insertion_Sort_List.py
+++ lc/linked_list/147_Insertion_Sort_List.py
@@ -5,11 +5,13 @@ class Solution:
     def insertionSortList(self, head: ListNode) -> ListNode:
         if not head:
             return head
+        # 创建一个新链表
         helper = ListNode(0)
         pre = helper
         cur = head
         while cur:
             next = cur.next
+            # 在新链表中找到插入的位置
             while pre.next and cur.val > pre.next.val:
                 pre = pre.next
             cur.next = pre.next
