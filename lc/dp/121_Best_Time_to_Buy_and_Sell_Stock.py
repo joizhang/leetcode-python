1fca8530a6d2a084b03916e2f1719c7214c579a4
joizhang <zxlwhom@outlook.com>
2021-07-20 20:07:37 +0800

    121_Best_Time_to_Buy_and_Sell_Stock

--- lc/dp/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ lc/dp/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -9,8 +9,17 @@ class Solution:
             profit = max(profit, price - cost)
         return profit
 
+    def maxProfit2(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [0] * n
+        min_price = prices[0]
+        for i in range(1, n):
+            min_price = min(min_price, prices[i])
+            dp[i] = max(dp[i - 1], prices[i] - min_price)
+        return dp[-1]
+
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.maxProfit([7, 1, 5, 3, 6, 4]))
-    print(s.maxProfit([7, 6, 4, 3, 1]))
+    print(s.maxProfit2([7, 1, 5, 3, 6, 4]))
+    print(s.maxProfit2([7, 6, 4, 3, 1]))
