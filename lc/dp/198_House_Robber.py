83c8042906d3bbb4a8e761937efdef69969d6b89
joizhang <zxlwhom@outlook.com>
2021-07-20 16:34:21 +0800

    198_House_Robber

--- lc/dp/198_House_Robber.py
+++ lc/dp/198_House_Robber.py
@@ -3,9 +3,12 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums) + 1
-        dp = [0] * n
+        n = len(nums)
+        dp = [0] * (n + 1)
         dp[1] = nums[0]
         for i in range(2, n):
-            dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1])
+            dp[i] = max(
+                nums[i - 1] + dp[i - 2],  # 抢当前的
+                dp[i - 1]                 # 不抢当前的
+            )
         return dp[-1]
