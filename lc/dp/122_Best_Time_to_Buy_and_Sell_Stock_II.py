e9587460acc2972817481d8346d9768c36ff5ad9
joizhang <zxlwhom@outlook.com>
2021-07-20 20:09:18 +0800

    122_Best_Time_to_Buy_and_Sell_Stock_II

---
+++ lc/dp/122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 所有上涨交易日都买卖
+        n = len(prices)
+        dp = [0] * n
+        for i in range(1, n):
+            if prices[i] > prices[i - 1]:
+                dp[i] = prices[i] - prices[i - 1] + dp[i - 1]
+            else:
+                dp[i] = dp[i - 1]
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maxProfit([7, 1, 5, 3, 6, 4]))
+    print(s.maxProfit([1, 2, 3, 4, 5]))
+    print(s.maxProfit([7, 6, 4, 3, 1]))
