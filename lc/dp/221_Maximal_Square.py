d49bf52765ee0b0285331a56d006356a4034ce86
joizhang <zxlwhom@outlook.com>
2021-08-13 19:08:12 +0800

    221_Maximal_Square.py

---
+++ lc/dp/221_Maximal_Square.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        """
+        https://leetcode.com/problems/maximal-square/discuss/600149/Python-Thinking-Process-Diagrams-DP-Approach
+        :param matrix:
+        :return:
+        """
+        if not matrix:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        max_side = 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if matrix[i - 1][j - 1] == '1':
+                    dp[i][j] = min(
+                        dp[i - 1][j - 1],
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    ) + 1
+                    max_side = max(max_side, dp[i][j])
+        return max_side * max_side
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.maximalSquare([["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]))
+    print(s.maximalSquare([["0", "1"], ["1", "0"]]))
