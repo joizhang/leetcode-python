10b4f5b2950f177de0a4e2f815fd9da66224afbb
joizhang <zxlwhom@outlook.com>
2021-08-14 18:19:10 +0800

    215_Kth_Largest_Element_in_an_Array.py

--- lc/sort/215_Kth_Largest_Element_in_an_Array.py
+++ lc/sort/215_Kth_Largest_Element_in_an_Array.py
@@ -40,6 +40,7 @@ class Solution:
         # nums[lo+1...i] <= v <= nums[j...hi]
         i, j = lo + 1, hi
         while True:
+            # 出错的地方：使用的<=
             while i <= hi and nums[i] < v:
                 i += 1
             while j >= lo + 1 and nums[j] > v:
@@ -47,10 +48,12 @@ class Solution:
             if i > j:
                 break
             nums[i], nums[j] = nums[j], nums[i]
+            # 出错的地方：没有下面两行
             i += 1
             j -= 1
         # 此时nums[i] > v 且 nums[j] < v，所以将lo和v位置的进行交换
         nums[lo], nums[j] = nums[j], nums[lo]
+        # 出错的地方：没有使用j
         self.quick_sort(nums, lo, j - 1)
         self.quick_sort(nums, j + 1, hi)
 
