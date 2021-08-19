ea42e72ff6683383d29f2b93ab7e1273ee5bd3e5
joizhang <zxlwhom@outlook.com>
2021-08-19 19:06:29 +0800

    classification.

--- lcof/backtracking/Offer_17.py
+++ lcof/backtracking/Offer_17.py
@@ -28,30 +28,21 @@ class Solution:
 
 
 class Solution2:
-    nine = 0
-    start = 0
-
-    def backtrack(self, n, x, num, ans):
-        if x == n:
-            s = ''.join(num[self.start:])
-            if s != '0':
-                ans.append(s)
-            if n - self.start == self.nine:
-                self.start -= 1
+    def backtrack(self, n, track, ans):
+        if n == 0:
+            num = int(''.join(track))
+            if num != 0:
+                ans.append(num)
             return
 
         for i in range(10):
-            if i == 9:
-                self.nine += 1
-            num[x] = str(i)
-            self.backtrack(n, x + 1, num, ans)
-        self.nine -= 1
+            track.append(str(i))
+            self.backtrack(n - 1, track, ans)
+            track.pop()
 
     def printNumbers(self, n: int) -> List[int]:
-        num, ans = ['0'] * n, []
-        self.nine = 0
-        self.start = n - 1
-        self.backtrack(n, 0, num, ans)
+        ans = []
+        self.backtrack(n, [], ans)
         return ans
 
 
@@ -60,4 +51,4 @@ if __name__ == '__main__':
     # print(s.printNumbers(2))
 
     s = Solution2()
-    print(s.printNumbers(3))
+    print(s.printNumbers(2))
--- lcof/two_pointer/Offer_58-I.py
+++ lcof/two_pointer/Offer_58-I.py
@@ -3,9 +3,7 @@ class Solution:
         s_split = s.strip().split()
         lo, hi = 0, len(s_split) - 1
         while lo < hi:
-            tmp = s_split[lo]
-            s_split[lo] = s_split[hi]
-            s_split[hi] = tmp
+            s_split[lo], s_split[hi] = s_split[hi], s_split[lo]
             lo += 1
             hi -= 1
         return ' '.join(s_split)
