bd57b219c618d1b0bec5f463bfb3b79b6d4d78a8
joizhang <zxlwhom@outlook.com>
2021-08-26 20:17:02 +0800

    ZJ19

--- nowcoder/dp/ZJ19 万万没想到之抓捕孔连顺.py
+++ nowcoder/dp/ZJ19 万万没想到之抓捕孔连顺.py
@@ -9,25 +9,32 @@ def solve(n, d, nums):
     dp = [0] * n
     dp[2] = 1 if nums[2] - nums[0] <= d else 0
     for i in range(3, n):
-        c = 0
+        count = 0
         j = i - 1
-        while j >= 0 and nums[j] + d >= nums[i]:
-            c += 1
+        while j >= 0 and nums[i] - nums[j] <= d:
+            count += 1
             j -= 1
-        dp[i] += c * (c - 1) // 2 if c >= 3 else 0
+        dp[i] += count * (count - 1) // 2 if count >= 3 else 0
         dp[i] %= MOD
     return sum(dp) % MOD
 
 
 def solve2(n, d, nums):
+    """
+    改进算法,nums是递增的,j不用每次都重新计算,类似滑动窗口
+    dp[i]: 以nums[i]为最大元素的方案数
+    i: 遍历到的数字
+    j: 与i相差距离>d的最近的坐标索引
+    时间复杂度O(n) ac
+    """
     dp = [0] * n
     dp[2] = 1 if nums[2] - nums[0] <= d else 0
     j = 0
     for i in range(3, n):
-        while j < i and nums[j] + d < nums[i]:
+        while j < i and nums[i] - nums[j] > d:
             j += 1
-        c = i - j
-        dp[i] += c * (c - 1) // 2 if c >= 3 else 0
+        count = i - j
+        dp[i] += count * (count - 1) // 2 if count >= 3 else 0
         dp[i] %= MOD
     return sum(dp) % MOD
 
@@ -35,4 +42,4 @@ def solve2(n, d, nums):
 N, D = map(int, input().split())
 nums = list(map(int, input().split()))
 # print(nums)
-print(solve(N, D, nums))
+print(solve2(N, D, nums))
