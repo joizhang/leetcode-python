cc6dfde80ed54f77bf8f629fe1dfe18e5a5dee1b
joizhang <zxlwhom@outlook.com>
2021-08-30 15:24:54 +0800

    1014_Best_Sightseeing_Pair.py

--- lc/dp/game/1014_Best_Sightseeing_Pair.py
+++ lc/dp/game/1014_Best_Sightseeing_Pair.py
@@ -5,8 +5,9 @@ class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         n = len(values)
         ans = 0
+        # 要求 values[i] + values[j] + i - j 最大
+        # 变量 pre_max 记录当前元素 A[j] 之前的 A[i] + i 的最大值
         pre_max = values[0] + 0
-        # 要求values[i] + values[j] + i - j最大
         for j in range(1, n):
             ans = max(ans, pre_max + values[j] - j)
             pre_max = max(pre_max, values[j] + j)
