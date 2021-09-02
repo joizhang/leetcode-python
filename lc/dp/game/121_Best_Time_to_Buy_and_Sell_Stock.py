35e71c4b05780084d770fbca6cef7dfb052f16be
joizhang <zxlwhom@outlook.com>
2021-09-02 20:09:47 +0800

    121_Best_Time_to_Buy_and_Sell_Stock.py

--- lc/dp/game/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ lc/dp/game/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -16,6 +16,7 @@ class Solution:
         for i in range(1, n):
             min_price = min(min_price, prices[i])
             dp[i] = max(dp[i - 1], prices[i] - min_price)
+            # print(dp)
         return dp[-1]
 
 
