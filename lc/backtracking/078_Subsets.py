e685182b6523d7fa33b5afb4d4e599449b46a2c1
joizhang <zxlwhom@outlook.com>
2021-09-22 21:04:56 +0800

    078_Subsets.py

--- lc/backtracking/078_Subsets.py
+++ lc/backtracking/078_Subsets.py
@@ -2,17 +2,18 @@ from typing import List
 
 
 class Solution:
+    """
+    数组中的元素 互不相同
+    """
     def backtrack(self, nums, start, track, ans):
         ans.append(track.copy())
-
-        if start == len(nums):
-            return
         for i in range(start, len(nums)):
             track.append(nums[i])
             self.backtrack(nums, i + 1, track, ans)
             track.pop()
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         ans = []
         self.backtrack(nums, 0, [], ans)
         return ans
