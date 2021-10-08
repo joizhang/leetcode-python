8670948117a276dba9f3df20cea1bab5ba523a7c
joizhang <zxlwhom@outlook.com>
2021-10-08 21:28:10 +0800

    NC51_合并k个已排序的链表.py

---
+++ nowcoder/NC51_合并k个已排序的链表.py
@@ -0,0 +1,41 @@
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+#
+#
+# @param lists ListNode类一维数组
+# @return ListNode类
+#
+from utils import ListNode
+
+
+class Solution:
+    def merge_two_list(self, l1, l2):
+        dummy = ListNode()
+        move = dummy
+        while l1 and l2:
+            if l1.val <= l2.val:
+                move.next = l1
+                l1 = l1.next
+            else:
+                move.next = l2
+                l2 = l2.next
+            move = move.next
+        move.next = l1 if l1 else l2
+        return dummy.next
+
+    def merge_lists(self, lists, lo, hi):
+        if lo == hi:
+            return lists[lo]
+        mid = (lo + hi) // 2
+        l1 = self.merge_lists(lists, lo, mid)
+        l2 = self.merge_lists(lists, mid + 1, hi)
+        return self.merge_two_list(l1, l2)
+
+    def mergeKLists(self, lists):
+        # write code here
+        if not lists:
+            return None
+        return self.merge_lists(lists, 0, len(lists) - 1)
