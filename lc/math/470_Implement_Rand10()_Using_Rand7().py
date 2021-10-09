ef4e0a4106cbaaf763476fa218816756a3920f3b
joizhang <zxlwhom@outlook.com>
2021-10-09 19:33:18 +0800

    470_Implement_Rand10()_Using_Rand7().py

--- lc/math/470_Implement_Rand10()_Using_Rand7().py
+++ lc/math/470_Implement_Rand10()_Using_Rand7().py
@@ -1,14 +1,38 @@
 # The rand7() API is already defined for you.
 # def rand7():
 # @return a random integer in the range 1 to 7
+import random
+
+
+def rand7():
+    return random.randint(1, 7)
+
 
 class Solution:
+
     def rand10(self):
         """
         :rtype: int
         """
+        num = (rand7() - 1) * 7 + rand7()
+        while num > 40:
+            num = (rand7() - 1) * 7 + rand7()
+        return 1 + num % 10
+
+    def rand10_(self):
+        """
+        :rtype: int
+        """
         while True:
-            ans = 7 * (rand7() - 1) + rand7()
-            if ans <= 40:
-                break
-        return (ans-1) % 10 + 1
+            num = (rand7() - 1) * 7 + rand7()
+            # 如果在40以内，那就直接返回
+            if num <= 40:
+                return 1 + num % 10
+            # 说明刚才生成的在41-49之间，利用随机数再操作一遍
+            num = (num - 40 - 1) * 7 + rand7()
+            if num <= 60:
+                return 1 + num % 10
+            # 说明刚才生成的在61-63之间，利用随机数再操作一遍
+            num = (num - 60 - 1) * 7 + rand7()
+            if num <= 20:
+                return 1 + num % 10
