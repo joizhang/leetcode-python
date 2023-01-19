2513855dcc1d311c9c918b11baec5760a336ad7e
joizhang <zxlwhom@outlook.com>
2023-01-19 20:10:14 +0800

    feat: 23. 合并K个升序链表

--- lc/linked_list/023_Merge_k_Sorted_Lists.py
+++ lc/linked_list/023_Merge_k_Sorted_Lists.py
@@ -1,8 +1,8 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import List
+
+from utils import ListNode
+
+
 class Solution:
     def merge_two_list(self, l1, l2):
         dummy = ListNode()
