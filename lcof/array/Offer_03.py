f7e726009af56d79cbccaad8f692295f76d747d0
joizhang <zxlwhom@outlook.com>
2023-07-08 12:25:25 +0800

    feat: 剑指 Offer 03. 数组中重复的数字

--- lcof/array/Offer_03.py
+++ lcof/array/Offer_03.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # 哈希表
     def findRepeatNumber(self, nums: List[int]) -> int:
         dd = {}
         for num in nums:
@@ -10,7 +11,16 @@ class Solution:
             dd[num] = 1
         return -1
 
+    # 排序
     def findRepeatNumber2(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return nums[i]
+        return -1
+
+    # 原地交换
+    def findRepeatNumber3(self, nums: List[int]) -> int:
         i = 0
         while i < len(nums):
             num = nums[i]
@@ -28,5 +38,8 @@ class Solution:
 if __name__ == '__main__':
     s = Solution()
     print(s.findRepeatNumber([2, 3, 1, 0, 2, 5, 3]))
+    print(s.findRepeatNumber([3, 4, 2, 0, 0, 1]))
     print(s.findRepeatNumber2([2, 3, 1, 0, 2, 5, 3]))
     print(s.findRepeatNumber2([3, 4, 2, 0, 0, 1]))
+    print(s.findRepeatNumber3([2, 3, 1, 0, 2, 5, 3]))
+    print(s.findRepeatNumber3([3, 4, 2, 0, 0, 1]))
