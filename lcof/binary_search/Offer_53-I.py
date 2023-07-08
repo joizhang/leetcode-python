e38b201ff797501f59cfe12365eb5a7d73f89aa8
joizhang <zxlwhom@outlook.com>
2023-07-08 12:30:31 +0800

    feat: 剑指 Offer 53 - I. 在排序数组中查找数字 I

--- lcof/binary_search/Offer_53-I.py
+++ lcof/binary_search/Offer_53-I.py
@@ -3,6 +3,20 @@ from typing import List
 
 class Solution:
 
+    def search(self, nums: List[int], target: int) -> int:
+        ans = [-1, -1]
+        if len(nums) == 0:
+            return 0
+
+        pos = self.find_fisrt_pos(nums, target)
+        if pos == -1:
+            return 0
+        ans[0] = pos
+
+        pos = self.find_last_pos(nums, target)
+        ans[1] = pos
+        return ans[1] - ans[0] + 1
+
     def find_fisrt_pos(self, nums, target):
         lo, hi = 0, len(nums) - 1
         while lo < hi:
@@ -29,16 +43,8 @@ class Solution:
             return hi
         return -1
 
-    def search(self, nums: List[int], target: int) -> int:
-        ans = [-1, -1]
-        if len(nums) == 0:
-            return 0
 
-        pos = self.find_fisrt_pos(nums, target)
-        if pos == -1:
-            return 0
-        ans[0] = pos
-
-        pos = self.find_last_pos(nums, target)
-        ans[1] = pos
-        return ans[1] - ans[0] + 1
+if __name__ == '__main__':
+    s = Solution()
+    print(s.search([5, 7, 7, 8, 8, 10], 8))
+    print(s.search([5, 7, 7, 8, 8, 10], 6))
