90a0ceea25f5a1d873be8a2092c29662bf8cafa0
joizhang <zxlwhom@outlook.com>
2023-07-08 15:14:41 +0800

    feat: 剑指 Offer 11. 旋转数组的最小数字

--- lcof/binary_search/Offer_11.py
+++ lcof/binary_search/Offer_11.py
@@ -2,16 +2,25 @@ from typing import List
 
 
 class Solution:
+    """
+    剑指 Offer 11. 旋转数组的最小数字
+    """
     def minArray(self, numbers: List[int]) -> int:
         lo, hi = 0, len(numbers) - 1
+        # 在一个可能存在重复元素值的数组 numbers 寻找旋转点，
+        # 旋转点就是最小值
         while lo < hi:
             mid = lo + (hi - lo) // 2
-            while lo < mid:
-                if numbers[lo] != numbers[hi]:
-                    break
-                lo += 1
-            if numbers[mid] >= numbers[lo] >= numbers[hi]:
+            if numbers[mid] > numbers[hi]:
                 lo = mid + 1
-            else:
+            elif numbers[mid] < numbers[hi]:
                 hi = mid
+            else:
+                hi -= 1
         return numbers[lo]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.minArray([3, 4, 5, 1, 2]))
+    print(s.minArray([2, 2, 2, 0, 1]))
