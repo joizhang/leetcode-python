2a80ed1fba0769b88a56765272fb1e0860039130
joizhang <zxlwhom@outlook.com>
2023-07-09 15:34:16 +0800

    feat: 剑指 Offer 63. 股票的最大利润

--- lcof/dp/game/Offer_63.py
+++ lcof/dp/game/Offer_63.py
@@ -2,6 +2,8 @@ from typing import List
 
 
 class Solution:
+    """剑指 Offer 63. 股票的最大利润"""
+
     def maxProfit(self, prices: List[int]) -> int:
         cost, profit = float('+inf'), 0
         for price in prices:
